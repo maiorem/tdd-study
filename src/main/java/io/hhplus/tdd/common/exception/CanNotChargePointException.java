df3ddb34886b2595fe4205346f8417a52b6ff289
Seyoung Hong <maiorem00@gmail.com>
2024-09-26 10:58:35 +0900

    feature: 예외 설정

---
+++ src/main/java/io/hhplus/tdd/common/exception/CanNotChargePointException.java
@@ -0,0 +1,7 @@
+package io.hhplus.tdd.common.exception;
+
+public class CanNotChargePointException extends RuntimeException{
+    public CanNotChargePointException(String message) {
+        super(message);
+    }
+}
